1cf1e8139ac0c4473dd10831a9a8e2134b5da665
Bruno Fischetti <brunofischetti@gmail.com>
2018-06-20 20:07:11 -0300

    created models for sales

--- models/sale.py
+++ models/sale.py
@@ -6,3 +6,18 @@ class SaleModel(db.Model):
 
     sale_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     total = db.Column(db.Float(precision=2))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+    user = db.relationship('UserModel')
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def json(self):
+        return {'sale_id': self.sale_id,
+                'total': self.total,
+                'user_id': self.user_id}
