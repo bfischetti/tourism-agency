d3847a58e7e6010856d9820de3885641fc7a0995
Bruno Fischetti <brunofischetti@gmail.com>
2018-06-20 20:06:17 -0300

    remove unnecessary import

--- resources/product.py
+++ resources/product.py
@@ -1,5 +1,5 @@
 from flask_restful import Resource, reqparse
-from flask_jwt_extended import jwt_required, get_jwt_identity
+from flask_jwt_extended import jwt_required
 from models.product import ProductModel
 
 
