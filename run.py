cc74f93ed488cd9cae65dfbd2c3d063bd124883e
Bruno Fischetti <brunofischetti@gmail.com>
2019-02-02 19:32:42 -0300

    trying to enable db migrations

--- run.py
+++ run.py
@@ -1,4 +1,17 @@
 from app import app
+from db import db
 
-if __name__ == "__main__":
-    app.run()
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
+
+db.init_app(app)
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
+
+db.init_app(app)
+
+if __name__ == '__main__':
+    manager.run()
